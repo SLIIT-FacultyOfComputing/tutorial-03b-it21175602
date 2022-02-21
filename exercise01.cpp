d19189e3171853e89af224218b686be120bf329b
IT21175602 <99182172+it21175602@users.noreply.github.com>
2022-02-21 12:05:42 +0000

    lab

--- exercise01.cpp
+++ exercise01.cpp
@@ -8,6 +8,6 @@ int main() {
  for (int r = 0; r < 5; r++) {
      cout <<setw(5) <<r+1 
           <<setw(15)<<  names[r]
-          <<setw(10)<< marks[r] << endl;
+          <<setw(10)<<setiosflags(ios::fixed)<<setprecision(3)<< marks[r] << endl;
  }
 }
